bca28b2dc72ebd96a13619867c0ecb33011424d6
BACHUBHAVANA <143540134+BACHUBHAVANA@users.noreply.github.com>
2024-10-07 12:07:10 +0530

    Time: 0 ms (100%), Space: 40.1 MB (66.32%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.java
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -1,8 +1,17 @@
 class Solution {
+    public int solve(int n,int[] dp)
+    {
+        if(n==0)return 0;
+        if(n==1)return 1;
+        if(dp[n]!=-1)
+        return dp[n];
+        else 
+        return dp[n]=solve(n-1,dp)+solve(n-2,dp);
+    }
     public int fib(int n){
-       if(n==0)return 0;
-       if(n==1)return 1;
-       else return fib(n-1)+fib(n-2);
+      int[] dp=new int[n+1];
+      Arrays.fill(dp,-1);
+      return solve(n,dp);
     }
 }
-//time complexity=O(2^n) space=O(n)
+//time complexity=O(n) space=O(n)
